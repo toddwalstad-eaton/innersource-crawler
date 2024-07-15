abc8c92ba6a84c4d7fe536dfa332a392c2e52243
Todd Walstad <toddwalstad@eaton.com>
2024-07-15 11:39:04 -0400

    Remove stuff we don't need

--- crawler.py
+++ crawler.py
@@ -15,19 +15,15 @@ if __name__ == "__main__":
     load_dotenv(dotenv_path)
 
     # Auth to GitHub.com
-    ghe = os.getenv("GH_ENTERPRISE_URL", default="").strip()
-    if ghe:
-        gh = github3.github.GitHubEnterprise(ghe, token=os.getenv("GH_TOKEN"))
-    else:
-        gh = github3.login(token=os.getenv("GH_TOKEN"))
+    gh = github3.login(token=os.getenv("GH_TOKEN"))
 
     # Set the topic
-    topic = os.getenv("TOPIC")
-    if not topic:
-        raise ValueError("TOPIC environment variable not set")
+    #topic = os.getenv("TOPIC")
+    #if not topic:
+    #    raise ValueError("TOPIC environment variable not set")
 
     # If multiple topics, split topics by comma
-    topics = [t.strip() for t in topic.split(",")]
+    #topics = [t.strip() for t in topic.split(",")]
     organization = os.getenv("ORGANIZATION")
 
     # Create empty list for repos
@@ -36,60 +32,65 @@ if __name__ == "__main__":
     repo_set = set()
 
     # Iterate over topics, search for matching repositories, and process unique ones
-    for topic in topics:
-        search_string = "org:{} topic:{}".format(organization, topic)
-        all_repos = gh.search_repositories(search_string)
+    #for topic in topics:
+    search_string = "org:{} is:internal".format(organization)
+    all_repos = gh.search_repositories(search_string)
 
-        # For each repo in the search results, check if it's unique and add it to repo_set
-        for repo in all_repos:
-            if repo is not None and repo.repository.full_name not in repo_set:
-                repo_set.add(repo.repository.full_name)
-                print("{0}".format(repo.repository))
+    # For each repo in the search results, check if it's unique and add it to repo_set
+    for repo in all_repos:
+        if repo is not None and repo.repository.full_name not in repo_set:
+            repo_set.add(repo.repository.full_name)
+            print("{0}".format(repo.repository))
 
-                innersource_repo = repo.as_dict()
-                innersource_repo["_InnerSourceMetadata"] = {}
+            innersource_repo = repo.as_dict()
+            innersource_repo["_InnerSourceMetadata"] = {}
 
-                # fetch innersource.json
-                try:
-                    content = repo.repository.file_contents("/innersource.json").content
-                    metadata = json.loads(b64decode(content))
+            # fetch innersource.json
+            try:
+                content = repo.repository.file_contents("/innersource.json").content
+                metadata = json.loads(b64decode(content))
 
-                    innersource_repo["_InnerSourceMetadata"] = metadata
-                except github3.exceptions.NotFoundError:
-                    # innersource.json not found in repository, but it's not required
-                    pass
+                innersource_repo["_InnerSourceMetadata"] = metadata
+            except github3.exceptions.NotFoundError:
+                # innersource.json not found in repository, but it's not required
+                pass
 
-                # fetch repository participation
-                participation = repo.repository.weekly_commit_count()
+            # fetch repository participation
+            participation = repo.repository.weekly_commit_count()
+            innersource_repo["_InnerSourceMetadata"][
+                "participation"
+            ] = participation["all"]
+
+            # fetch contributing guidelines
+            try:
+                # if CONTRIBUTING.md exists in the repository, link to that instead of repo root
+                content = repo.repository.file_contents("/CONTRIBUTING.md").content
                 innersource_repo["_InnerSourceMetadata"][
-                    "participation"
-                ] = participation["all"]
+                    "guidelines"
+                ] = "CONTRIBUTING.md"
+            except github3.exceptions.NotFoundError:
+                # CONTRIBUTING.md not found in repository, but it's not required
+                pass
 
-                # fetch contributing guidelines
-                try:
-                    # if CONTRIBUTING.md exists in the repository, link to that instead of repo root
-                    content = repo.repository.file_contents("/CONTRIBUTING.md").content
-                    innersource_repo["_InnerSourceMetadata"][
-                        "guidelines"
-                    ] = "CONTRIBUTING.md"
-                except github3.exceptions.NotFoundError:
-                    # CONTRIBUTING.md not found in repository, but it's not required
-                    pass
+            # fetch repository topics
+            repo_topics = repo.repository.topics()
+            innersource_repo["_InnerSourceMetadata"]["topics"] = repo_topics.names
 
-                # fetch repository topics
-                repo_topics = repo.repository.topics()
-                innersource_repo["_InnerSourceMetadata"]["topics"] = repo_topics.names
+            # set description = '' if no exist
+            if innersource_repo['description'] is None:
+                innersource_repo['description'] = ''
 
-                # set description = '' if no exist
-                if innersource_repo['description'] is None:
-                    innersource_repo['description'] = ''
-
-                # calculate score
+            # calculate score
+            try:
                 innersource_repo["score"] = repo_activity.score.calculate(
                     innersource_repo
                 )
+            except Exception:
+                innersource_repo["score"] = 0
+                pass
+                # CONTRIBUTING.md not found in repository, but it's not required
 
-                repo_list.append(innersource_repo)
+            repo_list.append(innersource_repo)
 
     # Write each repository to a repos.json file
     with open("repos.json", "w") as f:
